6ac5744f43d84ed75a806075e788d3001cdfb8ce
zzd <zhaizhidong2021@163.com>
2023-02-04 22:23:32 +0800

    cache

---
+++ src/main/java/com/yupi/yupao/job/PreCacheJob.java
@@ -0,0 +1,54 @@
+package com.yupi.yupao.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.yupao.model.domain.User;
+import com.yupi.yupao.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Zhai Zhidong
+ * @version 1.0
+ * @Date 2023/2/4 19:28
+ * 缓存预热
+ */
+@Slf4j
+@Component
+public class PreCacheJob {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
+    List<Long> mainUserList = Arrays.asList(1L);
+
+    //每天执行，预热推荐用户
+    @Scheduled(cron = "30 50 19 * * *")
+    public void doCacheRecommendUser(){
+        for (Long userId: mainUserList) {
+            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+            String redisKey = String.format("yupao:user:recommend:%s",userId) ;
+            ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
+            //无缓存，去查数据库
+            //写缓存
+            try {
+                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+            } catch (Exception exception) {
+                log.error("redis key error",exception);
+            }
+        }
+
+    }
+}
